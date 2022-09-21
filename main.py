76742824d4e573104076f77bea40f355874c1d68
shuklasid19 <shuklasid19@gmail.com>
2022-09-21 20:23:03 +0530

    added some code

--- main.py
+++ main.py
@@ -4,13 +4,13 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+a = 34
+b = 34
+print(a+b)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+git
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
